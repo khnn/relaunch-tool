1d804fe7ca21a318ceb429d5deda4ba55e731dc7
Philipp Kohnen <hallo@dressedco.de>
2023-05-09 11:43:32 +0200

    update

--- components/LinkInput.tsx
+++ components/LinkInput.tsx
@@ -32,14 +32,14 @@ export default function LinkInput({ link, updateLinks }) {
   return (
     <>
       <div className="flex flex-col gap-2 mt-10">
-        <p>{JSON.stringify(currentLink)}</p>
         <div className="relative">
-          <input type='url' className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={currentLink.value} onChange={(e) => setCurrentLink({ id: currentLink.id, value: e.currentTarget.value })} />
-          {/* <button onClick={() => {
-          
-        }}>+</button> */}
+          <input type='url' className="block w-96 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={currentLink.value} onChange={(e) => setCurrentLink({ id: currentLink.id, value: e.currentTarget.value })} onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              fetchLink(currentLink)
+            }
+          }} />
           <div className="absolute right-3 top-3">
-            {checking && <FiCoffee />}
+            {checking && <FiCoffee className="animate-pulse" />}
             {!checking && currentLink.status === "success" && <FiCheck />}
             {!checking && currentLink.status === "error" && <FiX color="red" />}
           </div>
