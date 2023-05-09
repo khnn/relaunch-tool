6e05d5d6fba93db7bfe667349dd663fdc91538ef
Philipp Kohnen <hallo@dressedco.de>
2023-05-09 21:21:27 +0200

    linting

--- app/api/fetchLink/route.ts
+++ app/api/fetchLink/route.ts
@@ -1,13 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+
 export async function POST(request: Request) {
-    const data = await request.json()
-    const requestUrl = data.url
-  
-    try {
-      const response = await fetch(requestUrl, { method: 'HEAD' });
-      console.log(response)
-      return response;
-    } catch (error) {
-      console.log(error)
-      return error;
-    }
+  const data = await request.json()
+  const requestUrl = data.url
+
+  try {
+    const response = await fetch(requestUrl, { method: "HEAD" })
+    return response
+  } catch (error) {
+    return error
+  }
 }
--- app/api/fetchSitemap/route.ts
+++ app/api/fetchSitemap/route.ts
@@ -1,17 +1,21 @@
-import { NextResponse } from 'next/server';
+/* eslint-disable import/prefer-default-export */
+
+import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
-    const data = await request.json()
-    const requestUrl = data.url
-  
-    try {
-      const response = await fetch(requestUrl);
-      const xml = await response.text()
-      const filteredOutput = await xml.split("\n").filter((line) => line.includes("<loc>")).map((line) => line.replace("<loc>", "").replace("</loc>", ""))
+  const data = await request.json()
+  const requestUrl = data.url
 
-      return NextResponse.json({ routes: filteredOutput, message: "success" });
+  try {
+    const response = await fetch(requestUrl)
+    const xml = await response.text()
+    const filteredOutput = await xml
+      .split("\n")
+      .filter((line) => line.includes("<loc>"))
+      .map((line) => line.replace("<loc>", "").replace("</loc>", ""))
 
-    } catch (error) {
-      return NextResponse.json({ routes: [], message: "error" });
-    }
+    return NextResponse.json({ routes: filteredOutput, message: "success" })
+  } catch (error) {
+    return NextResponse.json({ routes: [], message: "error" })
+  }
 }
