db0794f9cc09d178987473e3d97ed05b2563acaa
Philipp Kohnen <hallo@dressedco.de>
2023-05-09 15:11:42 +0200

    rm new url syntax

--- components/CheckLinks.tsx
+++ components/CheckLinks.tsx
@@ -26,10 +26,10 @@ export default function CheckLinks() {
     }])
   }
 
-  const handleXMLParsing = async (url) => {
+  const handleXMLParsing = async (url: string) => {
     const response = await fetch("/api/fetchSitemap", {
       method: "POST",
-      body: JSON.stringify({ url: url })
+      body: JSON.stringify({ url: new URL(url) })
     },)
 
     const data = await response.json()
@@ -55,7 +55,7 @@ export default function CheckLinks() {
     <>
       <h1 className='text-3xl font-extrabold'>Check Paths</h1>
       <div className='flex gap-2 py-4'>
-        <input type='url' id="siteMapUrl" placeholder='Sitemap URL' className="block w-96 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={siteMapUrl?.href} onChange={(e) => setSiteMapUrl(new URL(e.currentTarget.value))}></input>
+        <input type='url' id="siteMapUrl" placeholder='Sitemap URL' className="block w-96 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={siteMapUrl} onChange={(e) => setSiteMapUrl(e.currentTarget.value)}></input>
         <button className="block rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-200" disabled={!siteMapUrl} onClick={() => handleXMLParsing(siteMapUrl)}>Parse XML</button>
       </div>
       <hr />
