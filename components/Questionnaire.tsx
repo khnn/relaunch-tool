ad795e8e5182d2e9fc89a47cb44b49607e5214ec
Philipp Kohnen <hallo@dressedco.de>
2023-06-05 19:07:03 +0200

    labels

--- components/Questionnaire.tsx
+++ components/Questionnaire.tsx
@@ -7,19 +7,40 @@ import { Progress } from "@/components/ui/progress"
 import { Slider } from "@/components/ui/slider"
 
 export default function Questionnaire() {
-  const [answers, setAnswers] = useState({
-    codeKnowledge: null,
-    contentChanges: null,
+  const [answers, setAnswers] = useState<{
+    codeKnowledge: [number]
+    contentChanges: [number]
+  }>({
+    codeKnowledge: [0],
+    contentChanges: [0],
   })
 
   const currentProgress =
     (answers.codeKnowledge ? 50 : 0) + (answers.contentChanges ? 50 : 0)
 
+  const knowledgeMapping = [
+    "I don't know any coding",
+    "I know a little bit",
+    "I know a lot",
+    "I'm a pro",
+    "I'm a master",
+  ]
+
+  const contentMapping = [
+    "Very few updates, maybe once a year",
+    "A few updates, maybe once a month",
+    "A lot of updates, maybe once a week",
+    "A lot of updates, maybe once a day",
+    "A lot of updates, maybe multiple times a day",
+  ]
   /* TODO
 
   - DB?
   - Hosting?
   - SSR? SPA? SSG?
+  - SEO?
+  - Fixed Hosting? Security Concerns?
+  - Labels
 
   */
 
@@ -33,25 +54,35 @@ export default function Questionnaire() {
         </TabsList>
         <TabsContent value="kwowledge">
           <Slider
-            value={[answers.codeKnowledge]}
+            value={answers.codeKnowledge}
             className="mt-8"
-            onValueChange={(value: number[]) => {
+            onValueChange={(value: [number]) => {
               setAnswers({ ...answers, codeKnowledge: value })
             }}
             min={0}
-            max={5}
+            max={4}
           />
+          {answers.codeKnowledge && (
+            <p className="mt-4 text-sm text-gray-500">
+              {knowledgeMapping[answers.codeKnowledge[0]]}
+            </p>
+          )}
         </TabsContent>
         <TabsContent value="content">
           <Slider
-            value={[answers.contentChanges]}
+            value={answers.contentChanges}
             className="mt-8"
-            onValueChange={(value: number[]) => {
+            onValueChange={(value: [number]) => {
               setAnswers({ ...answers, contentChanges: value })
             }}
             min={0}
-            max={5}
+            max={4}
           />
+          {answers.contentChanges && (
+            <p className="mt-4 text-sm text-gray-500">
+              {contentMapping[answers.contentChanges[0]]}
+            </p>
+          )}
         </TabsContent>
       </Tabs>
     </>
